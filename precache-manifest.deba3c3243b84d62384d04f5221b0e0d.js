85bcad416efaf02378a70314f5c1fd09d94f971f
faisalhatila <hatilas1994@gmail.com>
2021-10-17 03:11:03 +0500

    Updates

---
+++ precache-manifest.deba3c3243b84d62384d04f5221b0e0d.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00e757d6ad81eb38cf78e86b1381920",
+    "url": "/index.html"
+  },
+  {
+    "revision": "166306c1846a4b0f8579",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "e53c51e3b21562055d4f",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "4869ac4b0e3f9e677c61",
+    "url": "/static/css/main.47525d87.chunk.css"
+  },
+  {
+    "revision": "c7b138124c00940aa9f5",
+    "url": "/static/js/2.615d57a6.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/static/js/2.615d57a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "166306c1846a4b0f8579",
+    "url": "/static/js/3.84b22f73.chunk.js"
+  },
+  {
+    "revision": "e53c51e3b21562055d4f",
+    "url": "/static/js/4.57342755.chunk.js"
+  },
+  {
+    "revision": "4869ac4b0e3f9e677c61",
+    "url": "/static/js/main.0bd7c33a.chunk.js"
+  },
+  {
+    "revision": "2ca202dce607e06f2a38",
+    "url": "/static/js/runtime-main.6c82fff2.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "bbc6231ce2c857c66667b3813535ff14",
+    "url": "/static/media/HTML_CSS_and_Javascript_for_Web_Developers.bbc6231c.pdf"
+  },
+  {
+    "revision": "0f8006f57fcba7f2135d8bcfc4ef822a",
+    "url": "/static/media/JavaScript_Objects_Prototypes_and_Classes.0f8006f5.pdf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "04c1106576eb9bc54ddcfaad2311f33f",
+    "url": "/static/media/React_Big_Picture.04c11065.pdf"
+  },
+  {
+    "revision": "367f9d6f8e01033d73374d1a857f3f00",
+    "url": "/static/media/React_Hooks.367f9d6f.pdf"
+  },
+  {
+    "revision": "1b5a8bf521d0a262078a5999a8638827",
+    "url": "/static/media/React_and_Redux.1b5a8bf5.pdf"
+  },
+  {
+    "revision": "d5c2eff4d5adf78fb2f4a5d8bd068db4",
+    "url": "/static/media/airtech.d5c2eff4.png"
+  },
+  {
+    "revision": "3cac900aa3762e19e5b8d7939a9312ed",
+    "url": "/static/media/aptech.3cac900a.png"
+  },
+  {
+    "revision": "7da74bbe296a5c8d7f08165623aa24ee",
+    "url": "/static/media/axiom.7da74bbe.png"
+  },
+  {
+    "revision": "d05f5dbef8e14cfd47b3ddcdb403a1d7",
+    "url": "/static/media/cloudpeanuts.d05f5dbe.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "1209a0c9402e9ece9a77dd5fa9224f7b",
+    "url": "/static/media/myPhoto.1209a0c9.jpeg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0a801d654415ffa5731362649d0461af",
+    "url": "/static/media/softapps.0a801d65.jpg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
